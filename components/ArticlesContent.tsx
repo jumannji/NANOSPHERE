c1b9952eb42b391ba76385cc8284adeffc01999b
Nikoloz Gogava <nikolozgogava@Nikolozs-MacBook-Air.local>
2026-06-26 21:56:52 -0500

    add social links to expanded article view

--- components/ArticlesContent.tsx
+++ components/ArticlesContent.tsx
@@ -55,6 +55,20 @@ function MagOverlay({ article, onClose }: { article: Article; onClose: () => voi
           >
             Read
           </button>
+
+          <div className="mag-modal-socials">
+            {(['TIKTOK', 'INSTAGRAM', 'TWITTER', 'SUBSTACK'] as const).map(label => (
+              <a
+                key={label}
+                href="#"
+                className="mag-modal-social-link"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {label}
+              </a>
+            ))}
+          </div>
         </div>
       </div>
     </div>
